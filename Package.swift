bbc8e51c6332a2a7a440a84ac747b6fd4f31f786
Gavin Eadie <gavin@umich.edu>
2020-11-18 00:44:00 -0500

    Catch up after a long time away ..

--- Package.swift
+++ Package.swift
@@ -1,17 +1,26 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "mbox",
+
+    platforms: [
+        .macOS(.v10_10)
+    ],
+
     dependencies: [
-        .package(url: "https://github.com/apple/swift-tools-support-core.git", .branch("master")),
+        .package(url: "https://github.com/apple/swift-tools-support-core.git", from: "0.0.1"),
         .package(url: "https://github.com/JohnSundell/Files.git", from: "4.0.0"),
     ],
+
     targets: [
         .target(
             name: "mbox",
-            dependencies: ["SwiftToolsSupport-auto", "Files"]),
+            dependencies: [
+                .product(name: "SwiftToolsSupport-auto",
+                         package: "swift-tools-support-core"),
+                "Files"]),
         .testTarget(
             name: "mboxTests",
             dependencies: ["mbox"]),
